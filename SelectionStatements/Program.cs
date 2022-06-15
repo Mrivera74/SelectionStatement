c16ca2e14130a56389a97240dcbe483426487a2a
Mark Rivera <mrivera7475@gmail.com>
2022-06-15 15:16:55 -0400

    created templete for exercises

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Guess the Number");
+
+var r = new Random();
+
+var favNumber = r.Next(1, 1000);
+
+var userInput = int.Parse(Console.ReadLine());
+
+if (userInput == favNumber)
+{
+    Console.WriteLine("Correct!");
+}
+
+if (userInput > favNumber)
+{
+    Console.WriteLine("High");
+}
+
+
+else
+{
+    Console.WriteLine("Low");
+}
